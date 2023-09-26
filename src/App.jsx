da1b01bbc510fd457af129bc57184156a336ed58
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-09-26 13:35:59 +0200

    Renames AboutView

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./sass/main.scss";
 // Import some Bootstrap components
 import StartView from './views/StartView';
 import ConfirmedView from './views/ConfirmedView';
-import OmView from './views/OmView';
+import AboutView from './views/AboutView';
 import { Routes, Route} from "react-router-dom";
 import ViewHolder from "./ViewHolder";
 
@@ -15,7 +15,7 @@ export default function App() {
       <Route path="/" element={<ViewHolder />} >
         <Route index element={<StartView />} />
         <Route path="/ConfirmedView" element={<ConfirmedView />} />
-        <Route path="/OmView" element={<OmView />} />
+        <Route path="/AboutView" element={<AboutView />} />
 
         {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
