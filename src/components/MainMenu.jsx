25d36a452296f9f0f3260ea338f2327bd29b61e6
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-09-27 15:59:05 +0200

    Adjust logo size

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -15,7 +15,7 @@ export default function MainMenu() {
           <Container fluid>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Brand href="/StartView">
-                <div className="h4 text-secondary custom-text-logo text-center m-0">
+                <div className="h6 text-secondary custom-text-logo text-center m-0">
                   <img src="/public/img/logo/filmvisarna-logo-icon.png" className="d-block custom-logo-navbar mx-auto"></img>
                   Filmvisarna
                 </div>
