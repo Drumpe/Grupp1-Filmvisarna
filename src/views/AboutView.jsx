96f3d48b9858017445ce948676de89f553cfec4c
YasseinSaid <yassein99@gmail.com>
2023-09-28 09:57:56 +0200

    Center Image, Text allgined.
    Image centerd with auto, text center,
    Text aligned to wireframe.
    
    Card removed becuse off technical limit and hard coded.
    Ideas?

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -5,34 +5,38 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 const AboutView = () => {
 
     return (
-       
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Image src="holder.js/171x180" thumbnail />
-                    </Col>
-                </Row>
-                <Row>
-                <h1>Om oss</h1>
-                    <Col>
-                        <p>
-                            Välkommen till vår filmwebbplats! Vi brinner för film och vill dela vår kärlek till film med dig.
-                        </p>
-                        <p>
-                            Vårt uppdrag är att förse dig med den senaste filmen och information om kommande släpp. Vi hoppas kunna hjälpa dig att upptäcka nya.
-                        </p>
-                        <p>
-                            vårt team består av: Ola, Yassein, Patrik, Arsha, Albin, Jimmie
-                        </p>
-                        <Card className="mt-3">
-                            <Card.Title>Kontat info</Card.Title>
-                            <Card.Body>Filmvisarna@mail.com   <br /> 07000000 </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-       
 
+        <Container >
+            <Row> 
+                <Col className='mx-auto text-center'>
+                    <Image src="/public/img/Aboutposter/shakeee.jpg" thumbnail />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h1>Om oss</h1>
+                    <p>
+                        Välkommen till vår filmwebbplats! Vi brinner för film och vill dela vår kärlek till film med dig.
+                    </p>
+                    <p>
+                        Vårt uppdrag är att förse dig med den senaste filmen och information om kommande släpp. Vi hoppas kunna hjälpa dig att upptäcka nya filmer.
+                    </p>
+                    <p>
+                        vårt team består av: Ola, Yassein, Patrik, Arsha, Albin, Jimmie
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col >
+                    <div className="d-flex flex-column">
+                        <h5>Kontakt info</h5>
+                        <div>
+                            Filmvisarna@mail.com <br /> 07000000
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 
 }
