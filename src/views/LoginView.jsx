0631895d2c779f158edc5a73f20f7cc36fecdf88
Arshia257 <hosseiniarshia5@gmail.com>
2023-09-30 22:04:35 +0200

    Updaterad Användarenamn till E-postadress

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -14,13 +14,13 @@ export default function LoginView() {
       </div>
         <div className="my-3 mx-3">
           <Form>
-            <Form.Label>E-post</Form.Label>
+            <Form.Label>E-postadress</Form.Label>
             <Form.Control type="email" className='rounded-0'/>
             <Form.Label>Lösenord</Form.Label>
             <Form.Control type="Password" className='rounded-0'/>
           </Form>
         </div>
-      <p className='text-center'>Inte medlem ännu?<Link to="./RegisterView">Bli medlem</Link></p>
+      <p className='text-center'>Inte medlem ännu? <Link to="./RegisterView">Bli medlem</Link></p>
         <div className='d-flex justify-content-center my-5 mb-2'>
                 <Button variant="secondary" size="lg">
                     Logga in
