141e137470703965c59b4d49cc521a5ccf96928b
Arshia257 <hosseiniarshia5@gmail.com>
2023-09-30 10:49:29 +0200

    mockuploginviewupdate

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -4,13 +4,12 @@ import Form from 'react-bootstrap/Form';
 
 
 
-export default function MyComponent() {
+export default function LoginView() {
   return (
     <div>
       <div className="d-flex justify-content-around">
         <h1 className='text-center'>Logga in</h1>
       </div>
-      <div>
         <div className="my-3 mx-3">
           <Form>
             <Form.Label>Användarnamn</Form.Label>
@@ -19,8 +18,7 @@ export default function MyComponent() {
             <Form.Control type="Password" className='rounded-0'/>
           </Form>
         </div>
-      </div>
-      <p className='Custom-text-center'>Inte medlem ännu? <a href= "./NewAccountView">Bli medlem</a></p>
+      <p className='text-center'>Inte medlem ännu? <a href= "./NewAccountView">Bli medlem</a></p>
         <div className='d-flex justify-content-center my-5 mb-2'>
                 <Button variant="secondary" size="lg">
                     Logga in
