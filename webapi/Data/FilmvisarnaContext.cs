12df7936d1267d58008a75da089a5a1dfe879194
Patrik Ask <ask.patrik@hotmail.com>
2023-10-02 21:51:57 +0200

    Matching names with db

--- webapi/Data/FilmvisarnaContext.cs
+++ webapi/Data/FilmvisarnaContext.cs
@@ -7,23 +7,26 @@ namespace webapi.Data
     {
 
         public DbSet<Booking> bookings { get; set; }
-        public DbSet<BookingXSeat> bookingsXSeats { get; set; }
+        public DbSet<BookingXSeat> bookingsXseats { get; set; }
         public DbSet<Genre> genres { get; set; }
-        public DbSet<Movie> movies {get; set;}
-        public DbSet<MovieXGenre> moviesxgenres {get; set;}
-        public DbSet<PriceCategory> priceCategories {get; set;}
-        public DbSet<Screening> screenings {get; set;}
-        public DbSet<Seat> seats {get; set;}
-        public DbSet<Theater> theaters {get; set;}
-        public DbSet<User> users {get; set;}
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<MovieXGenre> moviesXgenres { get; set; }
+        public DbSet<PriceCategory> priceCategories { get; set; }
+        public DbSet<Screening> screenings { get; set; }
+        public DbSet<Seat> seats { get; set; }
+        public DbSet<Theater> theaters { get; set; }
+        public DbSet<User> users { get; set; }
 
-        public FilmvisarnaContext(DbContextOptions options) : base(options){ }
+        public FilmvisarnaContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-        modelBuilder.Entity<MovieXGenre>()
-        .HasKey(m => new { m.MovieId, m.GenreId });
-}
+        {
+            modelBuilder.Entity<MovieXGenre>()
+                .HasKey(m => new { m.MovieId, m.GenreId });
+
+            modelBuilder.Entity<BookingXSeat>()
+                .HasKey(bs => new { bs.BookingId, bs.SeatId });
+        }
 
     }
 }
