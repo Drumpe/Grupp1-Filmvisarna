3a34b5555e0b073f34523765069819fbcd71e9b9
Patrik Ask <ask.patrik@hotmail.com>
2023-10-03 12:51:10 +0200

    Extended the rendering of movie details

--- webapi/Controllers/MoviesController.cs
+++ webapi/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using webapi.Data;
 
@@ -18,18 +19,24 @@ namespace YourNamespace.Controllers
 
         //Get detailed movie view
         [HttpGet()]
-        public async Task<IActionResult> ListAllMovies()
+        public async Task<IActionResult> DetailedMoviesList()
         {
             var result = await _context.movies     
                 .Select(v => new
                 {   
                     Id = v.Id,
-                    Movie = v.Name,                              
+                    Movie = v.Name,   
+                    Genre = v.MoviesXGenres.Select(mxg => mxg.Genre.Name).FirstOrDefault(),
+                    Actors = JObject.Parse(v.Description)["actors"].ToObject<List<string>>(),
+                    Director = JObject.Parse(v.Description)["director"].ToString(),      
+                    Description = JObject.Parse(v.Description)["description"].ToString(), 
+                    ProductionYear = JObject.Parse(v.Description)["productionYear"].ToString(), 
+                    ProductionCountries = JObject.Parse(v.Description)["productionCountries"].ToObject<List<string>>(),
+                    Language = JObject.Parse(v.Description)["language"].ToString(),
+                    Subtitles = JObject.Parse(v.Description)["subtitles"].ToString(),                
                     Duration = v.Duration,
                     AgeLimit = v.AgeLimit,
-                    Description = JObject.Parse(v.Description)["description"].ToString(), 
-                    Genre = v.MoviesXGenres.Select(mxg => mxg.Genre.Name)
-                    .FirstOrDefault()
+                    //TODO: Reviews= JObject.Parse(v.Description)["reviews"].ToString()
                 })
                 .ToListAsync();
 
