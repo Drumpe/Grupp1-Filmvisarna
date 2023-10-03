8a9dfa8c6b8b5f656e5ee1a3033c729c0d240688
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-10-03 15:03:07 +0200

    Implements params

--- WebReact/src/views/MovieView.jsx
+++ WebReact/src/views/MovieView.jsx
@@ -6,66 +6,67 @@ import { Link } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, useParams } from 'react-router-dom';
 
 function BasicExample() {
-    const { movies } = useOutletContext();
+		const { movies } = useOutletContext();
+		const { movieId } = useParams();
 
-  return (
-    <Container >
-        <Row>
-            <Col className='d-flex justify-content-center'>
-                <Card >
-                    <Image style={{width: '35em', height: '20em'}} src="/img/poster/pulp_fiction.jpg"  fluid rounded />
-                </Card>
-            </Col>
-        </Row>
-        <Row>
-            <Col className=' d-flex justify-content-center'>
-                <Card className='bg-transparent text-light' style={{ width: '25rem' }}>
-                    <Card.Body>
-                    <Card.Title>{movies[3].movie}</Card.Title>
-                        <Card.Text className='mt-3'>{movies[3].description}</Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
-        <Row>
-            <Col className=' d-flex justify-content-center' >
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Datum
-                    </Dropdown.Toggle>
+	return (
+		<Container >
+				<Row>
+						<Col className='d-flex justify-content-center'>
+								<Card >
+										<Image style={{width: '35em', height: '20em'}} src="/img/poster/pulp_fiction.jpg"  fluid rounded />
+								</Card>
+						</Col>
+				</Row>
+				<Row>
+						<Col className=' d-flex justify-content-center'>
+								<Card className='bg-transparent text-light' style={{ width: '25rem' }}>
+										<Card.Body>
+										<Card.Title>{movies[movieId].movie}</Card.Title>
+												<Card.Text className='mt-3'>{movies[movieId].description}</Card.Text>
+										</Card.Body>
+								</Card>
+						</Col>
+				</Row>
+				<Row>
+						<Col className=' d-flex justify-content-center' >
+								<Dropdown>
+										<Dropdown.Toggle variant="success" id="dropdown-basic">
+												Datum
+										</Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">29/9</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">01/10</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">02/10</Dropdown.Item>
-                    </Dropdown.Menu>                                                                            
-                </Dropdown>
-            </Col>
-        </Row>
-        <Row>
-            <Col className=' d-flex justify-content-center'>
-                <Card className='mt-2 bg-transparent text-light' style={{ width: '25rem'} }>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item className='bg-transparent text-light'>Tid: 12:00 | Salong 1 </ListGroup.Item>
-                        <ListGroup.Item className='bg-transparent text-light'>Tid: 16:00 | Salong 2</ListGroup.Item>
-                    </ListGroup>
-                </Card>
-            </Col>
-        </Row>
-        <Row>
-            <Col className=' d-flex justify-content-center'>
-                <Link to='/TheaterView'>
-                    <Button className=' mt-2' variant="primary" style={{width: '25rem'}}>
-                        Välj visning
-                    </Button>{''}
-                </Link>
-            </Col>
-        </Row>
-    </Container>
-  );
+										<Dropdown.Menu>
+												<Dropdown.Item href="#/action-1">29/9</Dropdown.Item>
+												<Dropdown.Item href="#/action-2">01/10</Dropdown.Item>
+												<Dropdown.Item href="#/action-3">02/10</Dropdown.Item>
+										</Dropdown.Menu>                                                                            
+								</Dropdown>
+						</Col>
+				</Row>
+				<Row>
+						<Col className=' d-flex justify-content-center'>
+								<Card className='mt-2 bg-transparent text-light' style={{ width: '25rem'} }>
+										<ListGroup variant="flush">
+												<ListGroup.Item className='bg-transparent text-light'>Tid: 12:00 | Salong 1 </ListGroup.Item>
+												<ListGroup.Item className='bg-transparent text-light'>Tid: 16:00 | Salong 2</ListGroup.Item>
+										</ListGroup>
+								</Card>
+						</Col>
+				</Row>
+				<Row>
+						<Col className=' d-flex justify-content-center'>
+								<Link to='/TheaterView'>
+										<Button className=' mt-2' variant="primary" style={{width: '25rem'}}>
+												Välj visning
+										</Button>{''}
+								</Link>
+						</Col>
+				</Row>
+		</Container>
+	);
 }
 
 export default BasicExample;
