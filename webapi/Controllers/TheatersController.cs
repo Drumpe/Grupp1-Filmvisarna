e712fb40f31252cad902ff9bf6d22bf4b29de8e4
Patrik Ask <ask.patrik@hotmail.com>
2023-10-03 15:23:44 +0200

    Endpoint for theaters available seats

---
+++ webapi/Controllers/TheatersController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TheatersController : ControllerBase
+    {
+        private readonly FilmvisarnaContext _context;
+
+        public TheatersController(FilmvisarnaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _context.theaters
+                .Where(t => t.Id == id)
+                .Select(t => new
+                {
+                    Id = t.Id,
+                    Theater = t.Name,
+                    Seats = t.Seats.Select(s => new
+                    {
+                        SeatId = s.Id,
+                        Seat = s.seat,
+                        Row = s.Row,                      
+                        IsBooked = s.BookingXSeats.Any() ? 1 : 0
+                    })
+                })
+                .FirstOrDefaultAsync();
+                
+            return Ok(result);
+        }
+    }
+}
