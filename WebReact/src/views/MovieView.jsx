9b4012332126d6251649ebb3a6c89b56b6b1b076
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-10-04 21:50:01 +0200

    Removes unused class

--- WebReact/src/views/MovieView.jsx
+++ WebReact/src/views/MovieView.jsx
@@ -19,7 +19,7 @@ function BasicExample() {
 		<Container >
 			<Row>
 				<Col className='d-flex justify-content-center'>
-					<div className="w-100 ratio ratio-16x9 mw-100-lg">
+					<div className="w-100 ratio ratio-16x9">
 							<iframe width="100%" height="100%"
 								src={`https://www.youtube.com/embed/${movies[movieId].trailerURL}?autoplay=1&mute=1`}>
 							</iframe>
