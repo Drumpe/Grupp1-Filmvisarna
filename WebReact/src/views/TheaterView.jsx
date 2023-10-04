1d3b4a45db9c51e614cce5a7c78168583e1ef5c5
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-04 23:20:10 +0200

    Testar theaterview

--- WebReact/src/views/TheaterView.jsx
+++ WebReact/src/views/TheaterView.jsx
@@ -1,13 +1,43 @@
-import React, { useState, useEffect } from "react"
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useMemo } from "react"
 import { Container, Row, Col, ButtonToolbar, ButtonGroup, Button, CloseButton, Form } from 'react-bootstrap';
 import { get } from '../utilsAndHooks/rest';
+import { Link, useParams } from "react-router-dom";
 
 
 const MyComponent = () => {
     const [barnTickets, setBarnTickets] = useState(0);
     const [ordinareTickets, setOrdinareTickets] = useState(0);
     const [pensionarTickets, setPensionarTickets] = useState(0);
+    //const [theaterId, setTheaterId] = useState();
+    //const [screeningId, setScreeningId] = useState();
+    let { screeningId } = useParams();
+    //const [seatsArray, setSeatsArray] = useState([0]);
+    const [jsonTheater, setJsonTheater] = useState(() => fetchAndInit());
+
+    async function fetchAndInit() {
+        try {
+            var screeningJson = await get('screenings/' + screeningId);
+        } catch (err) {
+            console.log(err);
+        }
+        var theaterId = screeningJson.theaterId;
+        try {
+            var theaterJson = await get('theaters/' + theaterId);
+        } catch (err) {
+            console.log(err);
+        }
+
+        console.log("sId " + screeningId + " tId " + theaterId + ' ::: ' + theaterJson.theater);
+
+        //setJsonTheater(theaterJson);
+        //setSeatsArray(theaterJson.seats);
+        console.log("Teaternamnet får jag till " + theaterJson.theater + " men seatsArray är mycket dumt!");
+        console.log("Seat: " + theaterJson.seats[33].seat + " Row: " + theaterJson.seats[33].row);
+        //console.log("seatsArray[0].seatId: " + seatsArray[33].seat); //<--- krasch
+        //setJsonTheater(theaterJson);
+        
+        return theaterJson;
+    };
 
     const increaseCount = (category) => {
         switch (category) {
@@ -47,56 +77,64 @@ const MyComponent = () => {
         }
     };
 
+
     const ShowSeats = () => {
         const [result, setResult] = useState();
-        const [id, setId] = useState(1);
+
+        //var jsonString = await get('theaters/' + theaterId);
+
+        console.log("inside placeSeats " + jsonTheater.theater);
+        var seatsArray = jsonTheater.seats; // seatsArray[0] SeatId, [1] Seat, [2] Row , [3] Kanske blir bokad
+
+        const rowElements = useMemo(() => {
+            // Organisera seats by rows
+            const rows = {};
+            seatsArray.forEach((element) => {
+                if (!rows[element.row]) {
+                    rows[element.row] = [];
+                }
+                rows[element.row].push(element);
+            });
+
+
+            return Object.keys(rows).map((rowNumber) => (
+                <Row key={rowNumber}>
+                    {/*<Col className="col-2">Rad {rowNumber}</Col>*/}
+                    <Col className="d-flex justify-content-center">
+                        <ButtonToolbar className="mb-2" aria-label="Toolbar with Button groups">
+                            <ButtonGroup className="me-2" aria-label="First group">
+                                {rows[rowNumber].map((seatElement) => (
+                                    <Button variant="secondary me-2" key={seatElement.seatId}>{seatElement.seat}</Button>
+                                ))}
+                            </ButtonGroup>
+                        </ButtonToolbar>
+                    </Col>
+                </Row >
+            ));
+        }, [jsonTheater]); //End rowElements
+
 
         useEffect(() => {
-            async function fetchData() {
-                try {
-                    var jsonString = await get('theaters/' + id);
-                    var seatsArray = jsonString.seats; // seatsArray[0] SeatId, [1] Seat, [2] Row , [3] Kanske blir bokad
+            setResult(
+                <div>
+                    <h3 className="text-center">{jsonString.theater}</h3>
+                    <br />
+                    {rowElements}
+                </div>
+            );
+        }, [rowElements]);
 
-                    // Organisera seats by rows
-                    const rows = {};
-                    seatsArray.forEach((element) => {
-                        if (!rows[element.row]) {
-                            rows[element.row] = [];
-                        }
-                        rows[element.row].push(element);
-                    });
-
-                    const rowElements = Object.keys(rows).map((rowNumber) => (
-                        <Row key={rowNumber}>
-                            {/*<Col className="col-2">Rad {rowNumber}</Col>*/}
-                            <Col className="d-flex justify-content-center">
-                                <ButtonToolbar className="mb-2" aria-label="Toolbar with Button groups">
-                                    <ButtonGroup className="me-2" aria-label="First group">
-                                        {rows[rowNumber].map((seatElement) => (
-                                            <Button variant="secondary me-2" key={seatElement.seatId}>{seatElement.seat}</Button>
-                                        ))}
-                                    </ButtonGroup>
-                                </ButtonToolbar>
-                            </Col>
-                        </Row >
-                    ));
-                    setResult(
-                        <div>
-                            <h3 className="text-center">{jsonString.theater}</h3>
-                            <br/>
-                            {rowElements}
-                        </div>
-                    );
-                } catch (err) {
-                    console.log(err);
-                }
-            }
-            fetchData();
-        }, []);
         return <>{result}</>
-    }
+    }; //End ShowSeats
 
-    return (
+    //Debug
+    useEffect(() => {
+        console.log("UE " + jsonTheater.theater);
+    }, [jsonTheater]);
+
+    console.log("DeBuG: " + jsonTheater.length);
+
+    return jsonTheater.length < 1 ? null : (
         <Container className="mt-5">
 
             <Row>
