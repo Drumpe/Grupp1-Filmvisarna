3c13daa672365b61dfca87e9e2c7721e54361c47
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-05 16:21:18 +0200

    2 gets, wantedSeats och summaState

--- WebReact/src/views/TheaterView.jsx
+++ WebReact/src/views/TheaterView.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react"
+import React, { useState, useEffect, useMemo, useRef } from "react"
 import { Container, Row, Col, ButtonToolbar, ButtonGroup, Button, CloseButton, Form } from 'react-bootstrap';
 import { get } from '../utilsAndHooks/rest';
 import { Link, useParams } from "react-router-dom";
@@ -9,65 +9,119 @@ const TheaterView = () => {
     const [ordinareTickets, setOrdinareTickets] = useState(0);
     const [pensionarTickets, setPensionarTickets] = useState(0);
     let { screeningId } = useParams();
-    const [jsonTheater, setJsonTheater] = useState(null); //() => fetchAndInit()
+    const [jsonTheater, setJsonTheater] = useState(null);
+    const [jsonScreening, setJsonScreening] = useState(null);
+    const [wantedSeats, setWantedSeats] = useState([]);
+    const [summaState, setSummaState] = useState(0);
+
+    /* TODO:
+    *  Räkna summa rätt
+    *  toggle seat color in seatClicked
+    *  varför uppdateras inte wantedSeats direkt?
+    *  emailadressen skall sparas för att kunna skickas
+    *  POST booking
+    */
 
     async function fetchAndInit() {
         try {
             var screeningJson = await get('screenings/' + screeningId);
+            setJsonScreening(screeningJson);
             var theaterJson = await get('theaters/' + screeningJson.theaterId);
             return theaterJson;
         } catch (err) {
             console.log(err);
             return {};
-        } 
+        }
     };
 
     useEffect(() => {
         // Call the fetchAndInit function and set jsonTheater when the component mounts
         fetchAndInit().then((theaterJson) => {
-          setJsonTheater(theaterJson);
-          // Uncomment this line if you want to set seatsArray with theaterJson.seats
-          // setSeatsArray(theaterJson.seats);
+            setJsonTheater(theaterJson);
         });
-      }, []); // Empty dependency array to run the effect only once
+    }, []); // Empty dependency array to run the effect only once
 
     const increaseCount = (category) => {
         switch (category) {
             case 'barn':
                 setBarnTickets(barnTickets + 1);
+                checkVarAv();
                 break;
             case 'ordinare':
                 setOrdinareTickets(ordinareTickets + 1);
+                checkVarAv();
                 break;
             case 'pensionar':
                 setPensionarTickets(pensionarTickets + 1);
+                checkVarAv();
                 break;
             default:
                 break;
         }
     };
+
     const decreaseCount = (category) => {
         switch (category) {
             case 'barn':
                 if (barnTickets > 0) {
                     setBarnTickets(barnTickets - 1);
+
+                    checkVarAv();
                 }
                 break;
             case 'ordinare':
                 if (ordinareTickets > 0) {
                     setOrdinareTickets(ordinareTickets - 1);
+                    checkVarAv();
                 }
 
                 break;
             case 'pensionar':
                 if (pensionarTickets > 0) {
                     setPensionarTickets(pensionarTickets - 1);
+                    checkVarAv();
                 }
                 break;
             default:
                 break;
         }
+
     };
+
+    const seatClicked = (seatId) => {
+        console.log("In seatClicked " + wantedSeats.length );
+        // toggle wanted seat (toggla färg?)
+        //Om seatId in wantedSeats 
+        if (wantedSeats.find(x => x.seatId === seatId)) {
+            setWantedSeats((prevWantedSeats) => prevWantedSeats.filter(seat => seat.seatId !== seatId)); //Ta bort
+        } else {
+            setWantedSeats((prevWantedSeats) => [...prevWantedSeats, seatId]); //Lägg till
+        }
+        console.log("In seatClicked2 " + wantedSeats.length );
+        checkVarAv();
+    }
+
+    /* Skall kolla att Barn och Pensionärer inte överstiger antalet biljetter */
+    const checkVarAv = () => {
+        console.log("In checkVarAv");
+        if (barnTickets + pensionarTickets > wantedSeats.length) {
+            if (pensionarTickets > 0) {
+                decreaseCount('pensionar');
+            } else {
+                decreaseCount('barn');
+            }
+        }
+        raknaSumma();
+    }
+
+    const raknaSumma = () => {
+        console.log("In raknaSumma:  B: " + barnTickets + " P: " + pensionarTickets + " V: " + wantedSeats.length);
+        const barnPris = 80;
+        const pensionarPris = 120;
+        const vuxenPris = 140;
+        setSummaState((barnTickets * barnPris) + (pensionarTickets * pensionarPris) + ((wantedSeats.length - barnTickets - pensionarTickets) * vuxenPris));
+    }
+
 
     const ShowSeats = () => {
         const [result, setResult] = useState();
@@ -79,6 +133,16 @@ const TheaterView = () => {
                 if (!rows[element.row]) {
                     rows[element.row] = [];
                 }
+
+                //Här läggs till ett attribut till element, typ booked (boolean)
+                // om seatsArray.SeatId finns i jsonScreening.bookedSeats 
+                //  sätt element.booked till true annars false
+                if (!jsonScreening.bookedSeats.find(x => x.seatId === element.seatId)) {
+                    element.booked = true;
+                } else {
+                    element.booked = false;
+                }
+
                 rows[element.row].push(element);
             });
             return Object.keys(rows).map((rowNumber) => (
@@ -88,7 +152,15 @@ const TheaterView = () => {
                         <ButtonToolbar className="mb-2" aria-label="Toolbar with Button groups">
                             <ButtonGroup className="me-2" aria-label="First group">
                                 {rows[rowNumber].map((seatElement) => (
-                                    <Button variant="secondary me-2" key={seatElement.seatId}>{seatElement.seat}</Button>
+
+                                    //Här skall läggas till onclick så att den (seatId) läggs till i en list med säten_som_skall_bokas
+                                    //Färger borde fixas
+                                    <Button onClick={() => seatClicked(seatElement.seatId)}
+                                        variant={(seatElement.booked ? "primary" : "secondary") + " me-2"}
+                                        key={seatElement.seatId}
+                                        disabled={(!seatElement.booked )} >
+                                        {seatElement.seat}
+                                    </Button>
                                 ))}
                             </ButtonGroup>
                         </ButtonToolbar>
@@ -111,7 +183,6 @@ const TheaterView = () => {
 
     return !jsonTheater ? null : (
         <Container className="mt-5">
-
             <Row>
                 <Col className="col-5 text-end">
                     <Button variant="outline-secondary">
@@ -191,7 +262,7 @@ const TheaterView = () => {
             </Row>
             <Row>
                 <Col className="d-flex justify-content-center mt-3">
-                    <span style={{ fontSize: '22px' }}>Summa: </span>
+                    <span style={{ fontSize: '22px' }}>Summa: {summaState}</span>
                 </Col>
             </Row>
             <Row>
@@ -199,7 +270,6 @@ const TheaterView = () => {
                     <Link to='/ConfirmedView'>
                         <Button variant="secondary">Bekräfta bokning</Button>{' '}
                     </Link>
-
                 </Col>
             </Row>
         </Container>
