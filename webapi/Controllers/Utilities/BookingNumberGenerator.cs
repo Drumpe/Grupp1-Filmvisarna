cb41c4d2e0479ac377623ae7bc9c5531db748cd2
Patrik Ask <ask.patrik@hotmail.com>
2023-10-06 18:00:17 +0200

    changed to capital letters and shorter method name

--- webapi/Controllers/Utilities/BookingNumberGenerator.cs
+++ webapi/Controllers/Utilities/BookingNumberGenerator.cs
@@ -8,11 +8,11 @@ namespace webapi.Controllers.Utilities
     public static class BookingNumberGenerator
     {
         private static Random random = new Random();
-        private const string lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
         private const string digits = "0123456789";
-        public static string GenerateRandomBookingNumber()
+        public static string GenerateRandomNumber()
         {
-            var randomLetters = new string(Enumerable.Repeat(lowercaseLetters, 3)
+            var randomLetters = new string(Enumerable.Repeat(letters, 3)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
 
             var randomDigits = new string(Enumerable.Repeat(digits, 3)
