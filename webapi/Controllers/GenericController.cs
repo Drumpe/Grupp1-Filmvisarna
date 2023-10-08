ae034d9b7c363b2f085a7508a4dc64dab90f03dd
Patrik Ask <ask.patrik@hotmail.com>
2023-10-08 22:59:39 +0200

    added ///summaries to define the methods

--- webapi/Controllers/GenericController.cs
+++ webapi/Controllers/GenericController.cs
@@ -5,23 +5,30 @@ using webapi.Entities;
 
 namespace webapi.Controllers
 {
+
+    /// <summary>
+    /// A generic controller for performing basic CRUD operations (no joins) on entities of type T.
+    /// </summary>
+    /// <typeparam name="T">The entity type.</typeparam>
     [Route("api/[controller]")]
     [ApiController]
     public class GenericController<T> : ControllerBase where T : class, IEntity
     {
         public readonly FilmvisarnaContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the CrudController class with a database context.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public GenericController(FilmvisarnaContext context)
         {
             _context = context;
         }
-
-        // Get routes
-
-        public async Task<T> GetRowByIdAsync(int id)
-        {
-
-            return await _context.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
-        }
+        
+        /// <summary>
+        /// Gets a list of all entities of type T.
+        /// </summary>
+        /// <returns> An ActionResult containing a list of entities, or NotFound if none found.</returns>
         [HttpGet]
         public async Task<ActionResult<List<T>>> GetAll()
         {
@@ -40,33 +47,38 @@ namespace webapi.Controllers
                 Console.WriteLine(ex.Message);
                 return StatusCode(500, "Internal Server Error");
             }
-
         }
 
+        /// <summary>
+        /// Gets an entity of type T by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to retrieve.</param>
+        /// <returns>An ActionResult containing the entity, or NotFound if not found.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<List<T>>> GetById(int id)
         {
             try
             {
                 var entity = await _context.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
+
                 if (entity == null)
                 {
                     return NotFound();
                 }
                 return Ok(entity);
             }
-
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return StatusCode(500, "Internal Server Error");
             }
-
         }
 
-        // Post route
-        // TODO : Duplicate Entries. MySQL throws "Duplicate Entries" when setting Unique constraint. Easiest way to handle it.
-        // Otherwise AnyAsync, but has its limits.
+        /// <summary>
+        /// Creates a new entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to create.</param>
+        /// <returns>An ActionResult containing the created entity, or an error status code if unsuccessful.</returns>
         [HttpPost()]
         public async Task<ActionResult<T>> Post(T entity)
         {
@@ -83,27 +95,47 @@ namespace webapi.Controllers
             }
         }
 
-        // Delete route 
+        /// <summary>
+        /// Deletes an entity of type T by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to delete.</param>
+        /// <returns>An ActionResult with a success status code if the entity is deleted, or NotFound if not found.</returns>
         [HttpDelete("{id}")]
         public async Task<ActionResult<T>> DeleteById(int id)
         {
-            var entity = await GetRowByIdAsync(id);
-            _context.Remove(entity);
-
             try
             {
+                var entity = await GetByIdAsync(id);
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+                _context.Remove(entity);
                 await _context.SaveChangesAsync();
+                return NoContent();
             }
-            catch (DbUpdateException /* ex */)
+            catch (Exception ex)
             {
-                // Log the error (uncomment ex variable name and write a log.
-                return Conflict();
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
             }
-
-            return NoContent();
-
         }
-
+        /// <summary>
+        /// Gets an entity of type T by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to retrieve.</param>
+        /// <returns>An ActionResult containing the entity, or NotFound if not found.</returns>
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
+        }
+        
     }
-
 }
+
+
+      //TODO : Put/Patch
+    
+
+
+
