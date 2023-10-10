524d685c298507192da51b73fc10d277cdffbec6
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-10 20:20:06 +0200

    För validering av email

---
+++ WebReact/src/utilsAndHooks/validate.js
@@ -0,0 +1,6 @@
+function isValidEmail(email) {
+    // Regular expression for a basic email validation
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    
+    return emailRegex.test(email);
+}
