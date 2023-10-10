e7a26640d66d910fde967ebe62cd9f2210ab5e15
YasseinSaid <yassein99@gmail.com>
2023-10-10 11:44:25 +0200

    Första iterationen av password validation
    gjort så att den validerar med regex. enkelt. samt att den kontrollerar lössenordet on spot, och att man kan kolla på sitt lösenord.

--- WebReact/src/views/RegisterView.jsx
+++ WebReact/src/views/RegisterView.jsx
@@ -1,52 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import { isPasswordValid } from "../utilsAndHooks/PasswordValidate";
 
 export default function RegisterView() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
+    // kollar om lösenordet är valid samt om pasword är rört.
+    const [passwordValid, setPasswordValid] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [passwordTouched, setPasswordTouched] = useState(false);
+    
+    // kollar om lösenordet är valid samt om pasword är rört i real tid.
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        if (name === "password") {
+            validatePassword();
+        }
+    };
+    // kollar och validerar lösenordet refferar till paswsworValidate.js
+    const validatePassword = () => {
+        const isValid = isPasswordValid(formData.password);
+        setPasswordValid(isValid);
+        setPasswordTouched(true);
+    };
+    // till post senare
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add your registration logic here
+        console.log(formData);
+    };
+    const toggleShowPassword = () => {
+        setShowPassword(prevState => !prevState);
+    };
+
     return (
-        <>
-            <Container>
-                <Col className="mx-auto text-center">
-                    <Image src="/img/logo/filmvisarna-logo-icon.png" roundedCircle style={{ width: '100px', height: '100px' }} />
-                </Col>
-                <h1 className="text-center">Bli Medlem</h1>
-                <Form className="mx-auto">
-                    <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Förnamn</Form.Label>
-                        <Form.Control type="varchar" placeholder="Förnamn" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Efternamn</Form.Label>
-                        <Form.Control type="varchar" placeholder="Efternamn" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="JoeDoe@email.se" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Telefonnummer</Form.Label>
-                        <Form.Control type="varchar" placeholder="Telefonnummer" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                </Form>
-            </Container>
+        <Container>
             <Col className="mx-auto text-center">
-                <Button variant="primary" href="/LoginView" size="lg">
+                <Image src="/img/logo/filmvisarna-logo-icon.png" roundedCircle style={{ width: '100px', height: '100px' }} />
+            </Col>
+            <h1 className="text-center">Bli Medlem</h1>
+            <Form className="mx-auto" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formGroupFirstName">
+                    <Form.Label>Förnamn</Form.Label>
+                    <Form.Control type="text" name="firstName" placeholder="Förnamn" onChange={handleInputChange} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupLastName">
+                    <Form.Label>Efternamn</Form.Label>
+                    <Form.Control type="text" name="lastName" placeholder="Efternamn" onChange={handleInputChange} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>E-Post addres</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="JoeDoe@email.se" onChange={handleInputChange} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Lösenord</Form.Label>
+                    <Form.Control
+                        type={showPassword ? 'text' : 'password'}
+                        name="password"
+                        placeholder="Lösenord"
+                        onBlur={validatePassword}
+                        isInvalid={!passwordValid}
+                        onChange={handleInputChange}
+                    />
+                    <Button variant="outline-secondary" onClick={toggleShowPassword}>
+                        {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                    {!passwordValid && passwordTouched && (
+                        <Form.Control.Feedback type="invalid">
+                            Lösenordet måste vara 8 tecken långt, innehålla minst ett specialtecken och minst ett nummer.
+                        </Form.Control.Feedback>
+                    )}
+                </Form.Group>
+                <Button type="submit" variant="primary" size="lg" disabled={!passwordValid}> 
                     Registera
                 </Button>
-                <Button  variant="secondary link" href="/StartView" size="lg">
+                <Button variant="secondary link" href="/StartView" size="lg">
                     Avbryt
                 </Button>
-            </Col>
-        </>
+            </Form>
+        </Container>
     );
 }
-
-
