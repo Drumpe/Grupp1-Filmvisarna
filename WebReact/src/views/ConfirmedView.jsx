f0d86bbf0d9edf31009df9ae55e740f1b8c361de
Arshia257 <hosseiniarshia5@gmail.com>
2023-10-10 16:22:45 +0200

    Get från api samt design fixat

--- WebReact/src/views/ConfirmedView.jsx
+++ WebReact/src/views/ConfirmedView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
 import { get } from '../utilsAndHooks/rest';
 import { useParams } from 'react-router-dom';
 import { jsonDateToString } from '../utilsAndHooks/date-formatter';
@@ -10,11 +10,25 @@ export default function ConfirmedView() {
         const [seatfinder, setSeatFinder] = useState([]);
         const [formatedDate, setFormatedDate] = useState('');
 
+        
         async function fetchData() {
                var response = await get('/bookings/' + bookingId);
                setData(response);
                setSeatFinder(response.tickets)      
         }
+
+        function countTicketTypes(tickets) {
+                const counts = {};
+              
+                tickets.forEach(function (ticket) {
+                  const type = ticket.type;
+                  counts[type] = (counts[type] || 0) + 1;
+                });
+              
+                return counts;
+              }
+              
+        const ticketAmount = Object.entries(countTicketTypes(seatfinder)).map(([key, val]) => <li key={key}>{val} {key} biljett </li>)
 
         useEffect(() => {
                 fetchData();
@@ -28,11 +42,12 @@ return (
         <hr />
         <br />
         <h3 className='text-decoration-underline'>{data.theater}</h3>
-        <p>Antal:{" " + seatfinder.map(x => x.type).join(", ")}</p>
+        <p className='ticket-type-list'>Antal bilijeter: {ticketAmount}</p>
         <p>När: {formatedDate}</p>
-        <p>Plats: <em>{data.theater}, rad: {seatfinder.map(x => x.row)}, plats: {seatfinder.map(x => x.seat)}</em></p>
+        <p>Salong:{data.theater}</p>
+        <p>Plats: {seatfinder.map((x) => <li key={x.seatId}>rad {x.row}, stol {x.seat}</li>)}</p>
         <p>Bokningsnummer: <em>{data.bookingNumber}</em></p>
-        <p>Bekräftelsepost har skickats till <em>{data.email}</em></p>
+        <p>Ett bekräftelsepost har skickats till <em>{data.email}</em></p>
         <br />
         <hr />
         <p>Vänligen uppge bokningsnummer i kassan vid betalning</p>
