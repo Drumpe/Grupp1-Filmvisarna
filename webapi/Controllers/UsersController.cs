86ea0dacd450e3829dd866e14fda6b563921757c
Patrik Ask <ask.patrik@hotmail.com>
2023-10-10 10:32:50 +0200

    first version of hasing password at creation

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +17,31 @@ namespace webapi.Controllers
     {
         public UsersController(FilmvisarnaContext context) : base(context)
         {
+        }
+
+        [HttpPost("test")]
+        public async Task<IActionResult> RegisterUser(User newUser)
+        {
+
+            if (await _context.users.FirstOrDefaultAsync(u => u.EmailAdress == newUser.EmailAdress) is not null)
+                return BadRequest($"A user with the email address {newUser.EmailAdress} already exists in our system.");
+
+
+            newUser.Password = HashPassword(newUser.Password);
+            _context.users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
+        }
+
+
+        private string HashPassword(string password)
+        {
+            // convert password characters into binary according to UTF-8 encoding
+            var bytes = Encoding.UTF8.GetBytes(password);
+            // hashing algorithm that requires binary data
+            var hashedBytes = SHA256.HashData(bytes);
+            // resulting string: Base64-encoded representation of the SHA256 as a string
+            return Convert.ToBase64String(hashedBytes);
         }
     }
 }
