782af3f222647faaade15845995b804a04729938
Arshia257 <hosseiniarshia5@gmail.com>
2023-10-10 11:39:15 +0200

    Update date-formatter.js

--- WebReact/src/utilsAndHooks/date-formatter.js
+++ WebReact/src/utilsAndHooks/date-formatter.js
@@ -2,8 +2,3 @@ export function capitalize(dateAndTime) {
   let capitalized = `${dateAndTime.charAt(0).toUpperCase()}${dateAndTime.substring(1, dateAndTime.length)}`;
   return capitalized;
 }
-
-export function jsonDateToString(dateAndTime, options, timeZone) {
-  const BookingDateTime = new Date(dateAndTime);
-  return BookingDateTime.toLocaleDateString(timeZone, options);
-  }
