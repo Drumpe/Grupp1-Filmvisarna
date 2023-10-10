7c548abda9f4f864e7b7cbaf0ce71ee85f8d50c0
Patrik Ask <ask.patrik@hotmail.com>
2023-10-10 10:52:51 +0200

    removed users from generic so it has own path

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -1,47 +1,77 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webapi.Data;
 using webapi.Entities;
+using webapi.Controllers.Utilities;
 
 namespace webapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : GenericController<User>
+    public class UsersController : ControllerBase
     {
-        public UsersController(FilmvisarnaContext context) : base(context)
+        public readonly FilmvisarnaContext _context;
+        public UsersController(FilmvisarnaContext context)
         {
+            _context = context;
+
         }
 
-        [HttpPost("test")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            try
+            {
+                var entities = await _context.users.ToListAsync();
+                if (entities == null || !entities.Any())
+                {
+                    return NotFound();
+                }
+
+                return Ok(entities);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            try
+            {
+                var entity = await _context.users.SingleOrDefaultAsync(e => e.Id == id);
+
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+                return Ok(entity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPost()]
         public async Task<IActionResult> RegisterUser(User newUser)
         {
 
             if (await _context.users.FirstOrDefaultAsync(u => u.EmailAdress == newUser.EmailAdress) is not null)
                 return BadRequest($"A user with the email address {newUser.EmailAdress} already exists in our system.");
 
-
-            newUser.Password = HashPassword(newUser.Password);
+            newUser.Password = PasswordEncryptor.HashPassword(newUser.Password);
             _context.users.Add(newUser);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
         }
 
 
-        private string HashPassword(string password)
-        {
-            // convert password characters into binary according to UTF-8 encoding
-            var bytes = Encoding.UTF8.GetBytes(password);
-            // hashing algorithm that requires binary data
-            var hashedBytes = SHA256.HashData(bytes);
-            // resulting string: Base64-encoded representation of the SHA256 as a string
-            return Convert.ToBase64String(hashedBytes);
-        }
+
     }
 }
