17cd60b156b1161f9191ca0476c25fc10e27a788
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-11 13:23:59 +0200

    fix

--- WebReact/src/utilsAndHooks/validate.js
+++ WebReact/src/utilsAndHooks/validate.js
@@ -1,4 +1,4 @@
-function isValidEmail(email) {
+export default function isValidEmail(email) {
     // Regular expression for a basic email validation
     const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     
