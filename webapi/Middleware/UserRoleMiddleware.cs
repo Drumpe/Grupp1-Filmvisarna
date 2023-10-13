b5b5286f1a9f470af3ad3c5dea88c4d80be9da46
Patrik Ask <ask.patrik@hotmail.com>
2023-10-13 09:13:20 +0200

    custom middleware to set user role to "guest"

---
+++ webapi/Middleware/UserRoleMiddleware.cs
@@ -0,0 +1,26 @@
+using webapi.Controllers.Utilities;
+
+namespace webapi.Middleware
+{
+    public class UserRoleMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        // Constructor for the middleware, takes the next middleware as a parameter and returns a task
+        public UserRoleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        // The main method for the middleware, invoked when a request is processed.
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Set the user's role in the session to "guest."
+            context.Session.SetString("UserRole", UserRole.guest.ToString());
+
+            // Pass the request to the next middleware in the pipeline.
+            await _next(context);
+        }
+    }
+}
+
