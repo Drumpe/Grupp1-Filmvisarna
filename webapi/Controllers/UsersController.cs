a8a80ae790dc15f7942fa497e59896a7d7d74257
Patrik Ask <ask.patrik@hotmail.com>
2023-10-13 11:43:28 +0200

    styling code cleanup

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using webapi.Data;
 using webapi.Entities;
 using webapi.Controllers.Utilities;
-using System.Text.Json;
-using webapi.ViewModels;
 
 namespace webapi.Controllers
 {
@@ -39,7 +37,6 @@ namespace webapi.Controllers
             }
         }
 
-
         [HttpGet("{id}")]
         public async Task<ActionResult> GetById(int id)
         {
@@ -63,7 +60,6 @@ namespace webapi.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser(User newUser)
         {
-
             if (await _context.users.FirstOrDefaultAsync(u => u.EmailAdress == newUser.EmailAdress) is not null)
                 return BadRequest($"A user with the email address {newUser.EmailAdress} already exists in our system.");
 
@@ -73,6 +69,7 @@ namespace webapi.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
         }
+
         [HttpPost("login")]
         public async Task<IActionResult> Login(User userLogin)
         {
@@ -89,29 +86,22 @@ namespace webapi.Controllers
             }
 
             // Set session values as a logged in user
-
             HttpContext.Session.SetString("UserRole", user.UserRole);
             HttpContext.Session.SetString("Email", user.EmailAdress);
             HttpContext.Session.SetString("Name", $"{user.FirstName} {user.LastName}");
 
             // Authentication successful
             return Ok();
-
         }
+
         [HttpDelete("logout")]
         public IActionResult Logout()
         {
+            // Clear session values and set role to guest
+            HttpContext.Session.Clear();
+            HttpContext.Session.SetString("UserRole", UserRole.guest.ToString());
 
-        // Clear session values and set role to guest
-        HttpContext.Session.Clear();
-        HttpContext.Session.SetString("UserRole", UserRole.guest.ToString());
-
-        return NoContent();
-
+            return NoContent();
         }
-
-
-
-
     }
 }
