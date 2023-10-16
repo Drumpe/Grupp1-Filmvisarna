2d0d13b79473c015c3be05ddf6cc8b8f35131367
Patrik Ask <ask.patrik@hotmail.com>
2023-10-16 09:13:06 +0200

    fixed middleware so it updates correctly

--- webapi/Middleware/UserRoleMiddleware.cs
+++ webapi/Middleware/UserRoleMiddleware.cs
@@ -15,8 +15,12 @@ namespace webapi.Middleware
         // The main method for the middleware, invoked when a request is processed.
         public async Task InvokeAsync(HttpContext context)
         {
-            // Set the user's role in the session to "guest."
-            context.Session.SetString("UserRole", UserRole.guest.ToString());
+            // If the user is not a member
+            if (context.Session.GetString("UserRole") is null)
+            {
+                // Set the user's role in the session to "guest."
+                context.Session.SetString("UserRole", UserRole.guest.ToString());
+            }
 
             // Pass the request to the next middleware in the pipeline.
             await _next(context);
