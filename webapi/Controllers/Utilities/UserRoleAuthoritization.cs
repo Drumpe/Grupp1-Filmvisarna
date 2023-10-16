c16923f284dcd81e1e1915de8a632249128986e9
Patrik Ask <ask.patrik@hotmail.com>
2023-10-16 22:40:06 +0200

    implemented authorization for APIs

--- webapi/Controllers/Utilities/UserRoleAuthoritization.cs
+++ webapi/Controllers/Utilities/UserRoleAuthoritization.cs
@@ -1,32 +1,38 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using webapi.Data;
 
 namespace webapi.Controllers.Utilities
 {
-    public class UserRoleAuthoritization : AuthorizeAttribute, IAuthorizationFilter
+    public class AuthorizeUserRole : Attribute, IAuthorizationFilter
     {
-        public FilmvisarnaContext _context { get; }
-        public UserRoleAuthoritization(FilmvisarnaContext context)
-        {
-            _context = context;
-        }
-
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            var user = context.HttpContext.Session.GetString("UserRole");
-            var endpoint = "/movies";
-            //context.HttpContext.Request.Path.Value;
+            var dbContext = context.HttpContext.RequestServices.GetRequiredService<FilmvisarnaContext>(); //get db context (cant fetch via controller) filters not part of DI container by default
+            var userRole = context.HttpContext.Session.GetString("UserRole"); // get session user role
+            var endpoint = context.HttpContext.Request.Path.Value; // get the endpoint we call from
+            var httpMethod = context.HttpContext.Request.Method; // get the request method (get, post)
+            var requiredUserRole = "";
 
-            var acl = _context.authorizations.FirstOrDefault(a => a.Endpoint == endpoint);
+            try
+            {
+                // find the row in db that corresponds to endpoint + request method
+                var auth = dbContext.authorizations
+                .Include(a => a.UserRoles)
+                .FirstOrDefault(a => a.Endpoint == endpoint && a.HttpMethod == httpMethod);
 
-            var model = new {
-                Endpoint = acl.Endpoint,
-                Role = acl.UserRoles.Name
-            };
-
-            if (user != model.Role)
+                // try to set required userRole in variable
+                requiredUserRole = auth.UserRoles.Name;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Unauthorized");
+            }
+            // reject authorization if userrole doesnt match
+            if (userRole != requiredUserRole)
             {
                 context.Result = new UnauthorizedResult();
             }
