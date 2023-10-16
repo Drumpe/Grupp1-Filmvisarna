9d103d211df36e750f3cb1373afcf044e309a2fa
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-17 01:32:34 +0200

    Förbättrade responsiviteten

--- WebReact/src/components/ShowSeats.jsx
+++ WebReact/src/components/ShowSeats.jsx
@@ -14,18 +14,14 @@ export default function ShowSeats({ seats, theater, seatClicked }) {
         });
         return Object.keys(rows).map((rowNumber) => (
             <Row key={rowNumber}>
-                {/*<Col className="col-2">Rad {rowNumber}</Col>*/}
                 <Col className="d-flex justify-content-center">
-
                     {rows[rowNumber].reverse().map((seatElement) => (
-
                         <Image className="chairImage"
                             onClick={seatElement.booked ? null : () => seatClicked(seatElement.seatId)}
-                            src={(seatElement.booked ? "/img/ui/chair-red.svg" : seatElement.wanted ? "/img/ui/chair-blue.svg" : "/img/ui/chair-green.svg")} //Första försöket färg
+                            src={(seatElement.booked ? "/img/ui/chair-red.svg" : seatElement.wanted ? "/img/ui/chair-blue.svg" : "/img/ui/chair-green.svg")}
                             key={seatElement.seatId}
-                            />
+                        />
                     ))}
-
                 </Col>
             </Row >
         ));
@@ -34,7 +30,7 @@ export default function ShowSeats({ seats, theater, seatClicked }) {
     return <>
         <div>
             <h3 className="text-center">{theater.name}</h3>
-            <br />
+            <hr />
             {rowElements()}
         </div>
     </>;
