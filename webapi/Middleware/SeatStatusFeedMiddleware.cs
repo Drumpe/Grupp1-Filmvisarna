0347e3fe22f1f6bac1b05005d0d98f6bbf0b5ff1
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-10-16 15:45:23 +0200

    Adds allowed origins

--- webapi/Middleware/SeatStatusFeedMiddleware.cs
+++ webapi/Middleware/SeatStatusFeedMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Net.WebSockets;
 using System.Text;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Newtonsoft.Json;
 
 namespace webapi.Middleware
@@ -8,6 +9,10 @@ namespace webapi.Middleware
   {
 	private readonly RequestDelegate _next;
 	private readonly WebSocketConnectionManager _manager;
+	private static List<string> _allowedOrigins = new List<string> {
+		"http://localhost:5173/",
+		"https://localhost:5173/"
+	};
 
 	public SeatStatusFeedMiddleware(RequestDelegate next, WebSocketConnectionManager manager)
 	{
@@ -18,15 +23,16 @@ namespace webapi.Middleware
 	public async Task Invoke(HttpContext context) 
 	{
 		if (!context.WebSockets.IsWebSocketRequest) {
-		context.Response.StatusCode = 400;
-	}
+			await _next(context);
+		}
 
 	if (context.WebSockets.IsWebSocketRequest) {
 		// Validation, authorization etc, etc, etc
-
-		await AcceptAsync(context);
+		if (_allowedOrigins.Contains(context.Request.Headers.Origin))
+			await AcceptAsync(context);
 	} else {
 		context.Response.StatusCode = 403;
+		return;
 	}
 }
 
@@ -37,12 +43,13 @@ public async Task AcceptAsync(HttpContext context)
 	
 	Console.WriteLine($"Connection from ${connectionId} established...");
 
-	await RecieveMessageAsync(connection, async (result, buffer) => 
+	await RecieveMessageAsync(connection, async (result, buffer) =>
 	{
 		if (result.MessageType == WebSocketMessageType.Text) 
 		{
 			// input sanitization
-			// idea: whitelisting input? otherwise close connection? Something went wrong, redirect?
+			// idea: whitelisting input? otherwise close connection? Something went wrong, redirect
+
 			var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
 			Console.WriteLine($"Message recieved from {connectionId} ...");
 			Console.WriteLine($"Message: {message}");
@@ -56,7 +63,7 @@ public async Task AcceptAsync(HttpContext context)
 				Console.Error.Write($"Connection id: {connectionId} not found in manager.");
 			}
 
-			await _connection.CloseAsync(result.CloseStatus!.Value, result.CloseStatusDescription, CancellationToken.None);
+			await _connection.CloseOutputAsync(result.CloseStatus!.Value, result.CloseStatusDescription, CancellationToken.None);
 			Console.WriteLine($"({connectionId}), CloseStatus: {result.CloseStatus!.Value}");
 			return;
 		}
@@ -84,6 +91,18 @@ public async Task BroadcastJSONMessageAsync(string message)
 				WebSocketMessageType.Text, true, CancellationToken.None);
 		}
 	}
+}
+
+private async Task CloseClientAsync(string connectionId) 
+{
+	if (!_manager.GetAllConnections().TryRemove(connectionId, out WebSocket _connection)) {
+		Console.Error.Write($"Connection id: {connectionId} not found in manager.");
+		return;
+	}
+
+	await _connection.CloseOutputAsync(WebSocketCloseStatus.PolicyViolation, string.Empty, CancellationToken.None);
+	Console.WriteLine($"Connection from {connectionId} closed...");
+	return;
 }
 
 /*// Send connection id to client  
