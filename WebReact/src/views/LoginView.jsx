ba37b9ca8cffc9fefafb110776f99fafdca5176d
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-20 11:03:23 +0200

    Fixed LoginView

--- WebReact/src/views/LoginView.jsx
+++ WebReact/src/views/LoginView.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { post, get } from '../utilsAndHooks/rest';
+import { Button, Form, Col, Image} from 'react-bootstrap';
+import { post } from '../utilsAndHooks/rest';
 import { NavLink, useNavigate, useOutletContext } from 'react-router-dom';
 
 
 export default function LoginView() {
   let navigate = useNavigate();
-  const [{},setUser] = useOutletContext();
+  const [{ }, setUser] = useOutletContext();
   const [formData, setFormData] = useState({ email: '', password: '' });
 
-  const sendRequest = async () => {
+  const sendLoginRequest = async () => {
     /** Här borde göras kontroller innan vi skickar iväg och kolla att resultatet är ok */
     var login = {
       emailAdress: formData.email,
       password: formData.password
     }
-    await post('users/login', login);
+    let result = await post('users/login', login);
+    if (result.error) {
+      alert("Felaktig e-postadress eller passord.");
+      return;
+    }
     setUser();
     navigate("/");
-    //window.location.href = '/StartView/';
   };
+
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormData({
@@ -29,6 +33,9 @@ export default function LoginView() {
   }
   return (
     <div>
+      <Col className="mx-auto text-center">
+        <Image src="/img/logo/filmvisarna-logo-icon.png" roundedCircle style={{ width: '100px', height: '100px' }} />
+      </Col>
       <div className="d-flex justify-content-around">
         <h1 className='text-center'>Logga in</h1>
       </div>
@@ -42,7 +49,7 @@ export default function LoginView() {
       </div>
       <p className='text-center'>Inte medlem ännu? <NavLink to="/RegisterView">Bli medlem</NavLink></p>
       <div className='d-flex justify-content-center my-5 mb-2'>
-        <Button variant="secondary" size="lg" onClick={sendRequest}>
+        <Button variant="secondary" size="lg" onClick={sendLoginRequest}>
           Logga in
         </Button>
       </div>
