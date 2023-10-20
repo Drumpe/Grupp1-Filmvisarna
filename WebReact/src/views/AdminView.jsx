3f1563b15aa30e3a3ea8e39328e39898c87d7785
Patrik Ask <ask.patrik@hotmail.com>
2023-10-20 15:20:19 +0200

    render booking details

--- WebReact/src/views/AdminView.jsx
+++ WebReact/src/views/AdminView.jsx
@@ -21,20 +21,38 @@ export default function AdminView() {
     const filtered = bookings.bookings.filter((booking) => booking.bookingNumber === bookingNumber);
     setFilteredBooking(filtered);
   }
+  let ticketSum = 0; 
 
-  return (<>
-
-    <label>Bokningsnummer</label>
-    <input type='text' placeholder='xxxxxx' onChange={
-      (e) => filterBookings(e.target.value)} />
-    {filteredBookings.map((booking, index) => (
-      <div key={index}>
-       
-
+  return (
+    <>
+    <h2>Hämta bokningsdetaljer</h2>
+      <label>Bokningsnummer: </label>
+      <input type='text' placeholder='xxxxxx' onChange={
+        (e) => filterBookings(e.target.value)}/>
+      {filteredBookings.map((booking, index) => (
+        <div key={index}>
+          {booking.firstName && booking.lastName ? (
+            <p>Bokad av: {booking.firstName} {booking.lastName}</p>
+          ) : null}
+          <p>E-postadress: {booking.email} </p>
+          <p>Film: {booking.movie} </p>
+          <p>Salong: {booking.theater} </p>
+          <p>Tid för visning: {(booking.screeningTime).replace('T', ' ').slice(0, -3)} </p>
+          <p><b>Biljetter:</b></p>
+          {booking.tickets.map((ticket, ticketIndex) => {
+            ticketSum += ticket.price; // Update ticketSum with the ticket price
+            return (
+              <li key={ticketIndex}>
+                {ticket.type}biljett, {ticket.price}kr
+                <p>rad: {ticket.row}, stol {ticket.seat}</p>
+              </li>
+            );
+          })}
+          <p>Summa: {ticketSum} kr</p>
         </div>
 
-    ))}
-  </>
+      ))}
+    </>
 
   );
 }
