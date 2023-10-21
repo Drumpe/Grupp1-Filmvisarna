3b21b9cfae0bcda71a1f99cf3a01bf96f2d84f33
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-21 23:28:11 +0200

    Tab fix

--- WebReact/src/utilsAndHooks/createBookingJson.js
+++ WebReact/src/utilsAndHooks/createBookingJson.js
@@ -5,14 +5,14 @@ export default function createBookingJson(seats, user, screeningId, tickets) {
   var priceCat = makePriceCategoriesArray(tickets);
   var index = 0;
   seats.forEach((elem) => {
-      if (elem.wanted) {
-          tmpBookingSeatsArr.push({ SeatId: elem.seatId, PriceCategoryId: priceCat[index++] });
-      }
+    if (elem.wanted) {
+      tmpBookingSeatsArr.push({ SeatId: elem.seatId, PriceCategoryId: priceCat[index++] });
+    }
   });
   const bookingData = {
-      EmailAdress: user.userRole === "guest" ? formData.email : user.email,
-      ScreeningId: screeningId,
-      BookingXSeats: tmpBookingSeatsArr,
+    EmailAdress: user.userRole === "guest" ? formData.email : user.email,
+    ScreeningId: screeningId,
+    BookingXSeats: tmpBookingSeatsArr,
   }
   return bookingData;
 }
@@ -20,13 +20,13 @@ export default function createBookingJson(seats, user, screeningId, tickets) {
 function makePriceCategoriesArray(tickets) {
   var result = [];
   for (let index = 0; index < tickets.pensioner; index++) {
-      result.push(3);
+    result.push(3);
   }
   for (let index = 0; index < tickets.child; index++) {
-      result.push(2);
+    result.push(2);
   }
   for (let index = 0; index < tickets.ordinary; index++) {
-      result.push(1);
+    result.push(1);
   }
   return result;
 }
