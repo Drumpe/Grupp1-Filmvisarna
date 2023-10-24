1ba873c13edde1b120835dfced84781135236900
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-24 20:52:20 +0200

    Lade till onClick={toggleMenu} till admin

--- WebReact/src/components/MainMenu.jsx
+++ WebReact/src/components/MainMenu.jsx
@@ -59,7 +59,7 @@ export default function MainMenu({ user, setUser }) {
                     </>
                     )  : user.userRole === "admin" ? (
                     <>
-                      <NavLink to="/AdminView/" className="nav-link">Admin</NavLink>
+                      <NavLink to="/AdminView/" className="nav-link" onClick={toggleMenu}>Admin</NavLink>
                       <NavLink to="/StartView/" className="nav-link" onClick={logout}>Logga ut</NavLink>
                     </>
                     ) :
