15461319bfa070a2044c71e04047f60735a095a7
Drumpe <96122414+Drumpe@users.noreply.github.com>
2023-10-25 09:49:55 +0200

    Snabb styling av aboutview

--- WebReact/src/views/AboutView.jsx
+++ WebReact/src/views/AboutView.jsx
@@ -1,19 +1,15 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
-const AboutView = () => {
-
+export default function AboutView() {
     return (
-
         <Container >
-            <Row> 
+            <Row>
                 <Col className='mx-auto text-center'>
                     <Image src="/img/Aboutposter/Aboutpic.jpg" thumbnail />
                 </Col>
             </Row>
-            <Row>
-                <Col>
+            <Row  className='justify-content-center'>
+                <Col style={{maxWidth: '621px'}}>
                     <h1>Om oss</h1>
                     <p>
                         Välkommen till vår filmwebbplats! Vi brinner för film och vill dela vår kärlek till film med dig.
@@ -26,19 +22,14 @@ const AboutView = () => {
                     </p>
                 </Col>
             </Row>
-            <Row>
-                <Col >
-                    <div className="d-flex flex-column">
-                        <h5>Kontakt info</h5>
-                        <div>
-                            Filmvisarna@mail.com <br /> 07000000
-                        </div>
+            <Row className='justify-content-center'>
+                <Col style={{maxWidth: '621px'}}>
+                    <h5>Kontakt info</h5>
+                    <div>
+                        Filmvisarna@mail.com <br /> 07000000
                     </div>
                 </Col>
             </Row>
         </Container>
     );
-
 }
-
-export default AboutView;
