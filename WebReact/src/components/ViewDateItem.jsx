845187cd106d189a2a7ed53b5a2ca70294946093
Arshia257 <hosseiniarshia5@gmail.com>
2023-10-26 08:59:11 +0200

    progress

---
+++ WebReact/src/components/ViewDateItem.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import { get } from '../utilsAndHooks/rest';
+import { getLocaleDateString } from '../utilsAndHooks/formatter';
+import { useOutletContext } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { Row, Col, ListGroup } from 'react-bootstrap';
+
+
+
+export default function ViewDateItem() {
+
+  const [{ movies }] = useOutletContext();
+  const [screenings, setScreenings] = useState([]);
+  const [FoundScreening, setFoundScreening] = useState([])
+  const ref = useRef(null);
+  const [screeningDatesScrollPosition, setScreeningDatesScrollPosition] = useState(0);
+
+
+  useEffect(() => {
+    fetchData();
+
+  }, [])
+
+
+
+  const scrollScreeningDatesForward = () => {
+    const nextPos = screeningDatesScrollPosition + 250;
+    ref.current?.scroll({ top: 0, left: nextPos, behavior: "smooth" });
+    setScreeningDatesScrollPosition(nextPos);
+  };
+
+  const scrollScreeningDatesBackward = () => {
+    const nextPos = screeningDatesScrollPosition - 250;
+    nextPos < 250 ? 0 : nextPos;
+    ref.current?.scroll({ top: 0, left: nextPos, behavior: "smooth" });
+    setScreeningDatesScrollPosition(nextPos);
+  };
+
+  //Hämtar screenings
+  const fetchData = async () => {
+    var result = await get('Screenings')
+    setScreenings(result);
+  }
+  function getScreeningsByDate(screenings, givenDateAndTime) {
+    const formattedDate = givenDateAndTime.slice(0, 10); // Extract yyyy-mm-dd from givenDateAndTime
+    return screenings.filter(screening => {
+      const screeningDate = screening.dateAndTime.slice(0, 10); // Extract yyyy-mm-dd from the screening dateAndTime
+      return screeningDate === formattedDate;
+    });
+  }
+
+  const handleClick = (screening) => {
+    const items = getScreeningsByDate(screenings, screening.dateAndTime)
+    return setFoundScreening(items)
+  }
+
+  const displayFoundScreenings = () => {
+    return (
+
+      <ListGroup className="border-bottom-0" horizontal>
+        {FoundScreening.map(fs => {
+          const getHours = new Date(fs.dateAndTime).getHours();
+          const getMinutes = new Date(fs.dateAndTime).getMinutes();
+          const movieName = movies.find(m => m.id === fs.movieId)
+          return (
+            <ListGroup.Item key={fs.id} variant="secondary" className="screening-list-item" action href={`/MovieView/${fs.movieId}`}>
+              {`${getHours}:${getMinutes} ${movieName.movie}`}
+            </ListGroup.Item>
+          )
+        })}
+      </ListGroup>
+    )
+  }
+
+
+  const showScreenings = () => {
+    const uniqueDays = [];
+
+    const filteredScreenings = screenings.filter((x) => {
+      const today = new Date();
+      const date = new Date(x.dateAndTime);
+      const weekday = date.toLocaleDateString(date, { weekday: 'long' });
+
+      if (!uniqueDays.includes(weekday) && date >= today) {
+        uniqueDays.push(weekday);
+        return true;
+      }
+      return false;
+    });
+    return (
+      <>
+        {filteredScreenings.map((s) => {
+          return (
+            <ListGroup.Item
+              key={s.id}
+              className="rounded-bottom-0  w-100 pb-3"
+              variant="primary"
+              action
+              onClick={() => handleClick(s)}
+            >
+              {`${getLocaleDateString(s.dateAndTime, { month: 'numeric', day: 'numeric' })}
+                - ${getLocaleDateString(s.dateAndTime, { weekday: 'long' })}`}
+            </ListGroup.Item >
+          )
+        })}
+      </>
+
+    );
+  }
+
+  return <>
+    <Row>
+      <Col className="screening-dates d-flex justify-content-center">
+        <div ref={ref} className="w-100 overflow-x-scroll scrollbar-hidden">
+          {
+            screeningDatesScrollPosition > 25 &&
+            <div className="arrow-bg back-arrow" onClick={scrollScreeningDatesBackward}>
+              <div className="back-arrow-container d-flex justify-content-start align-items-center h-100">
+                <div className="back-arrow-content d-inline-flex justify-content-start align-items-center">
+                  <img className="arrow-scroll" src="/img/ui/ui-listgroup-backarrow.svg"></img>
+                </div>
+              </div>
+            </div>
+          }
+          {
+            screenings.length > 0 &&
+            <div className="arrow-bg forward-arrow" onClick={scrollScreeningDatesForward}>
+              <div className="forward-arrow-container d-flex justify-content-end align-items-center h-100">
+                <div className="forward-arrow-content d-inline-flex justify-content-end align-items-center">
+                  <img className="arrow-scroll" src="/img/ui/ui-listgroup-forwardarrow.svg"></img>
+                </div>
+              </div>
+            </div>
+          }
+          <ListGroup className="border-bottom-0" horizontal>
+            {showScreenings()}
+          </ListGroup>
+        </div>
+      </Col>
+      <Col style={he} className='width-auto justify-content-center'>
+        <div>
+          {displayFoundScreenings()}
+        </div>
+      </Col>
+    </Row>
+  </>
+
+}
--- WebReact/src/views/StartView.jsx
+++ WebReact/src/views/StartView.jsx
@@ -4,7 +4,7 @@ import { Row, Col, ListGroup } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useOutletContext } from 'react-router-dom';
 import { getLocaleDateString } from '../utilsAndHooks/formatter';
-import { get } from '../utilsAndHooks/rest';
+import ViewDateItem from '../components/ViewDateItem';
 
 export default function StartView() {
 
@@ -12,13 +12,9 @@ export default function StartView() {
     const [selectedAge, setSelectedAge] = useState(18);  // Default age
     const [filteredMovies, setFilteredMovies] = useState([]);
     const [screenings, setScreenings] = useState([]);
-    const [date, setDate] = useState();
-    const ref = useRef(null);
-    const [screeningDatesScrollPosition, setScreeningDatesScrollPosition] = useState(0);
 
 
     useEffect(() => {
-        fetchData();
         const filtered = movies.filter(movie => movie.ageLimit <= selectedAge);
         setFilteredMovies(filtered);
     }, [movies, selectedAge]);
@@ -27,126 +23,12 @@ export default function StartView() {
         setSelectedAge(Number(e.target.value));
     };
 
-    //Hämtar screenings
-    const fetchData = async () => {
-        var result = await get('Screenings')
-        setScreenings(result);
-    }
-
-    // const addScreenings = () => {
-    //     const array = [];
-    //     for (let i = 0; i < movies.length; i++) {
-    //         const movie = movies[i]
-    //         array.push({ screening: movie.screening, id: movie.id })
-
-    //     }
-    //     return setScreenings(array)
-    // }
-
-    const scrollScreeningDatesForward = () => {
-        const nextPos = screeningDatesScrollPosition + 250;
-        ref.current?.scroll({ top: 0, left: nextPos, behavior: "smooth" });
-        setScreeningDatesScrollPosition(nextPos);
-    };
-
-    const scrollScreeningDatesBackward = () => {
-        const nextPos = screeningDatesScrollPosition - 250;
-        nextPos < 250 ? 0 : nextPos;
-        ref.current?.scroll({ top: 0, left: nextPos, behavior: "smooth" });
-        setScreeningDatesScrollPosition(nextPos);
-    };
-
-    const showScreenings = () => {
-        const uniqueDays = [];
-
-        const filteredScreenings = screenings.filter((x) => {
-            const today = new Date();
-            const date = new Date(x.dateAndTime);
-
-            const weekday = date.toLocaleDateString(date, { weekday: 'long' });
-
-            //Kollar om veckodagen redan finns i UniqueDays array samt kollar om den dagen har passerat
-            if (!uniqueDays.includes(weekday) && date >= today) {
-                uniqueDays.push(weekday);
-                return true;
-            }
-            return false;
-        });
-        return (
-            <>
-                <ListGroup.Item
-                    className="rounded-bottom-0 w-100 pb-3"
-                    variant="primary"
-                    action
-                    onClick={() => setDate(null)}>
-                    Visa alla
-                </ListGroup.Item>
-                {filteredScreenings.map((s) => (
-                    <ListGroup.Item
-                        key={s.id}
-                        className="rounded-bottom-0  w-100 pb-3"
-                        variant="primary"
-                        action
-                        onClick={() => handleFilter(s.dateAndTime)}
-                    >
-                        {`${getLocaleDateString(s.dateAndTime, { month: 'numeric', day: 'numeric' })}
-                - ${getLocaleDateString(s.dateAndTime, { weekday: 'long' })}`}
-                    </ListGroup.Item>
-                ))}
-            </>
-
-        );
-    }
-
-    const handleFilter = (dateAndTime) => setDate(dateAndTime);
-
-    const sortedFilteredMovies = filteredMovies.filter((movie) => {
-        if (date == null) {
-            return movie
-        }
-        return movie.screening === date
-    });
-
-
-
     return (
         <>
             <Row>
                 <Col>
                     <h1 className="mb-4 text-primary d-inline-block">Visas nu</h1>
-                    <Row>
-                        <Col className="screening-dates d-flex justify-content-center">
-                            <div ref={ref} className="w-100 overflow-x-scroll scrollbar-hidden">
-                                {
-                                    screeningDatesScrollPosition > 55 &&
-                                    <div className="arrow-bg back-arrow" onClick={scrollScreeningDatesBackward}>
-                                        <div className="back-arrow-container d-flex justify-content-start align-items-center h-100">
-                                            <div className="back-arrow-content d-inline-flex justify-content-start align-items-center">
-                                                <img className="arrow-scroll" src="/img/ui/ui-listgroup-backarrow.svg"></img>
-                                            </div>
-                                        </div>
-                                    </div>
-                                }
-                                {
-                                    screenings.length > 0 &&
-                                    <div className="arrow-bg forward-arrow" onClick={scrollScreeningDatesForward}>
-                                        <div className="forward-arrow-container d-flex justify-content-end align-items-center h-100">
-                                            <div className="forward-arrow-content d-inline-flex justify-content-end align-items-center">
-                                                <img className="arrow-scroll" src="/img/ui/ui-listgroup-forwardarrow.svg"></img>
-                                            </div>
-                                        </div>
-                                    </div>
-                                }
-                                <ListGroup
-                                    title="Dropdown button"
-                                    variant="outline-secondary"
-                                    className="border-bottom-0"
-                                    horizontal>
-                                    {showScreenings()}
-                                </ListGroup >
-                            </div>
-                        </Col>
-                    </Row>
+                    <ViewDateItem />
                     <br />
                     <select
                         className="form-select text-secondary me-2 w-auto d-inline-block float-end"
@@ -163,7 +45,7 @@ export default function StartView() {
             </Row>
             <br />
             <Row className="align-items-center">
-                {sortedFilteredMovies.map(({ id, movie, images }) =>
+                {filteredMovies.map(({ id, movie, images }) =>
                     <Col className="col-6 col-lg-3 mb-4" key={id}>
                         <NavLink to={`/MovieView/${id}`} className="link-light link-underline-opacity-25 link-underline-opacity-75-hover">
                             <Card.Img className="top rounded ratio-6x9" alt={`${movie}`} src={`/img/poster/${images[0]}`} />
