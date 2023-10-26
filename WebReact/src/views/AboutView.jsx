acf85fa0e88b7daacc0a2db525be7bbcccecc5a2
adis gunic <adis.gunic01@gmail.com>
2023-10-26 08:03:39 +0200

    About view style + mer text

--- WebReact/src/views/AboutView.jsx
+++ WebReact/src/views/AboutView.jsx
@@ -6,36 +6,38 @@ const AboutView = () => {
 
     return (
 
-        <Container >
+        <Container className='text-center'>
             <Row> 
                 <Col className='mx-auto text-center'>
                     <Image src="/img/Aboutposter/Aboutpic.jpg" thumbnail />
                 </Col>
             </Row>
+            <div className=' container border border-light py-3 rounded-5 bg-info col-lg-8 mt-3'> 
             <Row>
-                <Col>
+                <Col className=' mt-3' >
                     <h1>Om oss</h1>
                     <p>
-                        Välkommen till vår filmwebbplats! Vi brinner för film och vill dela vår kärlek till film med dig.
+                        Välkommen till vår filmwebbplats, en plats där vår genuina passion för film och dess magiska värld är hjärtat i allt vi gör. Vi gläder oss åt att öppna våra dörrar och välkomna dig till vår gemenskap.
                     </p>
                     <p>
-                        Vårt uppdrag är att förse dig med den senaste filmen och information om kommande släpp. Vi hoppas kunna hjälpa dig att upptäcka nya filmer.
+                        Vårt uppdrag är djupt förankrat i att förse dig med den allra senaste informationen om filmvärlden. Vi strävar efter att ge dig en omfattande inblick i kommande filmutgåvor, nyheter och spännande projekt. Vi ser det som vår stora ära att hjälpa dig att upptäcka de pärlor som filmvärlden har att erbjuda.
                     </p>
                     <p>
-                        Vårt team består av: Ola, Yassein, Patrik, Arshia, Albin, Jimmie, Adis
+                        Bakom denna passionerade strävan finns vårt engagerade team, vars medlemmar är Ola, Yassein, Patrik, Arshia, Albin, Jimmie och Adis. Tillsammans delar vi vårt kunnande och vår kärlek till film med dig.
                     </p>
                 </Col>
             </Row>
             <Row>
-                <Col >
-                    <div className="d-flex flex-column">
-                        <h5>Kontakt info</h5>
-                        <div>
-                            Filmvisarna@mail.com <br /> 07000000
+                <Col>
+                    <div className="d-flex flex-column ">
+                        <h4>Kontakt info</h4>
+                        <div> 
+                          Du kan nå oss via e-post på <a href="#" >Filmvisarna@mail.com</a> eller ringa oss på 070-000 00 00. Vi ser fram emot att höra från dig och vara din guide in i den fantastiska världen av film.
                         </div>
                     </div>
                 </Col>
             </Row>
+            </div>
         </Container>
     );
 
