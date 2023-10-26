830d3a7f04fa5ac604a4c1a2efe45f3514f6ae3e
adis gunic <adis.gunic01@gmail.com>
2023-10-26 07:12:15 +0200

    Misnkade footer lite

--- WebReact/src/components/Footer.jsx
+++ WebReact/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Footer() {
 	return (
-		<footer className="footer mt-auto py-3 bg-info">
+		<footer className="footer mt-auto py-2 bg-info">
 			<div className='text-center p-3'>
         &copy; {new Date().getFullYear()} Copyright Filmvisarna
 		</div>
